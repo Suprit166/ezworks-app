be8212037106266ae1e830af2699963231a96535
Suprit Keni <supritkeni@gmail.com>
2024-06-11 13:01:01 +0530

    up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header';
-import ServiceCards from './components/ServiceCards';
-import ContactForm from './components/ContactForm';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import ServiceCards from "./components/ServiceCards";
+import ContactForm from "./components/ContactForm";
 
 const App = () => (
   <div className="app">
-    <Header />
-    <ServiceCards />
+    <div className="main">
+      <Header />
+      <ServiceCards />
+    </div>
     <ContactForm />
   </div>
 );
